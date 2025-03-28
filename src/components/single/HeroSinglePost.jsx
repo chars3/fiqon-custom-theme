c1baeba72d9f253497446a528ca35f001f67e48a
chars3 <charlesjoliviera65@gmail.com>
2025-03-28 14:05:01 -0300

    style(hero-single-post): improve responsive layout and spacing

--- src/components/single/HeroSinglePost.jsx
+++ src/components/single/HeroSinglePost.jsx
@@ -5,27 +5,27 @@ const HeroSinglePost = ({ post }) => {
   const { acf } = post;
 
   return (
-    <div className="bg-gradient-to-r from-green-100 to-gray-100 pt-16 pb-24 flex items-center justify-center">
-      <div className="w-full max-w-[1280px] flex flex-col md:flex-rol justify-between px-[24px] md:px-[40px] xl:px-[60px]">
+    <div className="bg-gradient-to-r from-green-100 to-gray-100 pt-16 md:pb-24 flex items-center justify-center">
+      <div className="w-full max-w-[1280px] flex flex-col md:flex-row justify-between px-[24px] md:px-[40px] xl:px-[60px]">
         {/* Texto e chamada para ação */}
         <div className="max-w-xl">
-          <h1 className="text-[32px] leading-[48px] font-bold text-text-300">
+          <h1 className="text-3xl md:text-[40px] font-bold text-text-300 text-center md:text-left">
             Integrar o <span className="text-primary">{acf.app_title}</span>{" "}
             aos seus sistemas nunca foi tão simples!
           </h1>
-          <p className="text-text-200 mt-4">
+          <p className="text-text-200 mt-8 md:mt-4 text-center md:text-left">
             Integre agora o {acf.app_title} com seus demais sistemas e coloque
             tarefas no piloto automático! Plataforma gratuita. Webhooks
             gratuitos.
           </p>
 
           {/* Botão */}
-          <button className="cursor-pointer mt-10 w-[464px] bg-green-600 text-white px-6 py-2 rounded-lg shadow-md hover:bg-green-700 transition">
+          <button className="cursor-pointer mt-10 w-full md:w-[464px] bg-green-600 text-white px-6 py-2 rounded-lg shadow-md hover:bg-green-700 transition">
             Integrar {acf.app_title}
           </button>
 
           {/* Benefícios */}
-          <div className="flex items-center mt-6 space-x-6 text-text-100 text-sm">
+          <div className="flex flex-col md:flex-row gap-2 md:gap-8 items-center mt-8 md:mt-6 text-text-100 text-sm">
             <span className="flex items-center">
               <Rocket className="text-text-100 mr-2" size={18} />É
               gratuito, por tempo ilimitado
@@ -38,7 +38,7 @@ const HeroSinglePost = ({ post }) => {
         </div>
 
         {/* Imagem */}
-        <div className="w-[50%] flex justify-center items-center">
+        <div className="md:w-[50%] flex justify-center items-center">
           <img
             src={acf.app_logo?.url}
             alt={acf.app_title}
